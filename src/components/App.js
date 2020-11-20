fa784382e293f44673124ebaf9f8a59daa8c35e1
Brycen Bartolome <brycenbartolome@gmail.com>
2020-11-20 13:35:06 -0800

    add container to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import KegControl from "./KegControl";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from "react-bootstrap";
 
 function App() {
   return (
     <>
       <Header />
-      <KegControl />
+      <Container>
+        <KegControl />
+      </Container>  
     </>
   );
 }
