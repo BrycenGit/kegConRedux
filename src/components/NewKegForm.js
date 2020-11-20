9454adcb7567ac3698389fbfeb5b3dd9793b6d7e
Brycen Bartolome <brycenbartolome@gmail.com>
2020-11-20 10:36:36 -0800

    add new Keg form content

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+import { v4 } from 'uuid';
+function NewKegForm(props) {
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onClickingSubmit({
+      name: event.target.name.value,
+      brand: event.target.brand.value,
+      price: parseInt(event.target.price.value),
+      quantity: parseInt(event.target.quantity.value),
+      id: v4(),
+    })
+  }
+
+  return(
+    <>
+      <ReusableForm
+      formSubmissionHandler={handleNewKegFormSubmission}
+      buttonText="Add Keg"/>
+    </>
+  );
+}
+
+NewKegForm.PropTypes = {
+  onClickingSubmit: PropTypes.func,
+}
+
+export default NewKegForm;
