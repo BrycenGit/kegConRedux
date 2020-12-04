710d1a58fa0c4b7011897ba76f274162a00e6fb0
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-04 09:35:01 -0800

    pass test forShould return default state if no action type is recognized

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,5 +1,5 @@
-import formVisibleReducer from '../../reducers/form-visible-reducer';
-import * as c from '../../actions/ActionTypes'
+import formVisibleReducer from '../../reducers/form-visible-reducer.js';
+import * as c from '../../actions/ActionTypes.js'
 
 
 describe("formVisibleReducer", () => {
