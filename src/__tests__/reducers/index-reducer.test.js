13baa4c35504fe3e4e0c822fdcfd4d72a64db84f
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-04 11:22:51 -0800

    pass test match default state of form visible reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,9 +1,9 @@
 import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
-// import formVisibleReducer from '../../reducers/form-visible-reducer';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
 import kegListReducer from '../../reducers/keg-list-reducer';
-// import editingReducer from '../../reducers/editing-reducer';
-// import * as c from '../../actions/ActionTypes'
+import editingReducer from '../../reducers/editing-reducer';
+import * as c from '../../actions/ActionTypes'
 
 let store = createStore(rootReducer);
 
@@ -18,6 +18,10 @@ describe('rootReducer', () => {
 
   test('Check that initial state of kegListReducer matches root reducer', () => {
     expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 
 });
