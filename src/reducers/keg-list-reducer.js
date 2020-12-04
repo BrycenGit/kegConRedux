cd2ddaa2967353912b0a507db6ca66236fba887b
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-04 10:35:22 -0800

    pass test should successfu;;y add new keg data to masterkegList

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,7 +1,20 @@
 import * as c from '../actions/ActionTypes'
 
 export default (state = {}, action) => {
+  const { name, brand, flavor, price, quantity, id } = action;
+
   switch (action.type) {
+    case c.ADD_KEG:
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          flavor: flavor,
+          price: price,
+          quantity: quantity,
+          id: id,
+        }
+      });
     default:
       return state;
   }
