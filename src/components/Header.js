47263f6cd7d59e7c33de3727cbe4b9bea041930d
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-14 07:53:49 -0800

    make changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {Navbar, Nav, Form, FormControl, Button} from "react-bootstrap"
-import PropTypes from "prop-types"
+import {Navbar} from "react-bootstrap"
+
 
 function Header(props) {
 
